0681965cf96716bc89bb2fe14be9ecbbd861d633
Vitor Galdino <httpvittor@gmail.com>
2023-04-18 23:15:52 -0300

    feat: add endpoint to access api documentation

--- bandkamp/urls.py
+++ bandkamp/urls.py
@@ -15,10 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
     path("api/", include("albums.urls")),
     # path("api/", include("songs.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
+    ),
 ]
